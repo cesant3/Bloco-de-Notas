e67bb96a0793ae63dfe5c460d214e37108ccd972
cesant3 <cesantiago3@gmail.com>
2021-04-01 22:25:03 -0300

    commit principal

---
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import './estilo.css'
+
+class ListaDeCategorias extends Component {
+    constructor() {
+        super()
+        this.state = { categorias: [] }
+    }
+
+    componentDidMount() {
+        this.props.categorias.inscrever(this._novasCategorias.bind(this))
+    }
+
+    _novasCategorias(categorias) {
+        this.setState({ ...this.state, categorias })
+    }
+
+    _handleInput(e) {
+        if (e.key === 'Enter') {
+            let valorCategoria = e.target.value
+            this.props.adicionarCategoria(valorCategoria)
+        }
+    }
+
+    render() {
+        return (
+            <section className='lista-categorias'>
+                <ul className='lista-categorias_lista'>
+                    {this.state.categorias.map((categoria, index) => {
+                        return <li key={index} className='lista-categorias_item' >{categoria}</li>
+                    })}
+                </ul>
+                <input
+                    type="text"
+                    className='lista-categorias_input'
+                    placeholder='Adicione uma categoria'
+                    onKeyUp={this._handleInput.bind(this)}
+                />
+            </section>
+        )
+    }
+}
+
+export default ListaDeCategorias
